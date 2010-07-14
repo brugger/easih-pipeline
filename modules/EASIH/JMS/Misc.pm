9859ba5f65f708ca7a3bd72dc38a16fbc5b263a2
Kim Brugger <kim.brugger@easih.ac.uk>
2010-07-14 14:30:36 +0100

    misc functions that does not fit elsewhere

---
+++ modules/EASIH/JMS/Misc.pm
@@ -0,0 +1,45 @@
+package EASIH::JMS::Misc;
+#
+# Misc functions that does not fit anywhere else, but multiple scripts rely on. 
+# 
+# 
+# 
+# Kim Brugger (13 Jul 2010), contact: kim.brugger@easih.ac.uk
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+use EASIH::JMS;
+
+
+
+# 
+# 
+# 
+# Kim Brugger (13 Jul 2010)
+sub find_program {
+  my ($program) = @_;
+
+
+  my @paths = ("/home/easih/bin/",
+	       "/home/kb468/bin/",
+	       "/usr/local/bin");
+  
+  foreach my $path ( @paths ) {
+    
+    return "$path/$program" if ( -e "$path/$program" );
+  }
+
+  my $location = `which $program`;
+  chomp( $location);
+  
+  return $location if ( $location );
+
+  return undef;
+}
+
+
+
+
+1;
